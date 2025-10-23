b9349b41e3a4675623459f7d5d6374fb62fc34e2
Janithsathsara123 <janithsathsara789@gmail.com>
2025-10-23 13:21:09 +0530

    Update pair.js

--- pair.js
+++ pair.js
@@ -486,7 +486,7 @@ socket.sendMessage(from, buttonMessage, { quoted: msg });
                     const seconds = Math.floor(uptime % 60);
 
     const captionText = `
-╭────◉◉◉────៚\n⏰ Bot Uptime: ${hours}h ${minutes}m ${seconds}s\n🟢 Active session: ${activeSockets.size}\n╰────◉◉◉────៚\n\n🔢 Your Number: ${number}\n\n*JANI-MD Main Website🌐*\n> https://JANI-md.pages.dev
+╭────◉◉◉────៚\n⏰ Bot Uptime: ${hours}h ${minutes}m ${seconds}s\n🟢 Active session: ${activeSockets.size}\n╰────◉◉◉────៚\n\n🔢 Your Number: ${number}\n\n*Jani-MD Main Website🌐*\n> https://Jani-md.pages.dev
 `;
 
     await socket.sendMessage(m.chat, {
