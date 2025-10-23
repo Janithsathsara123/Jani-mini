5b8c6b8d12a357275e0260eca1bdb20fcb5dd293
Janithsathsara123 <janithsathsara789@gmail.com>
2025-10-23 22:33:34 +0530

    Update pair.js

--- pair.js
+++ pair.js
@@ -1248,7 +1248,7 @@ break;
                         const regex = /(?:https?:\/\/)?(?:www\.)?(?:youtube\.com\/(?:watch\?v=|embed\/|v\/|shorts\/)|youtu\.be\/)([a-zA-Z0-9_-]{11})/;
                         const match = url.match(regex);
                         return match ? match[1] : null;
-						const apiUrl = `https://api.id.dexter.it.com/download/youtube-audio/audio?url=${encodeURIComponent(link)}`;
+						const apiUrl = `https://api.id.dexter.it.com/download/youtube-audio/songdown?url=${encodeURIComponent(link)}`;
         const { data } = await axios.get(apiUrl);
                     }
 
