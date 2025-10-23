3c132b9850c735bf5ec8861546e51dd938c82bed
Janithsathsara123 <janithsathsara789@gmail.com>
2025-10-24 00:16:21 +0530

    Update pair.js

--- pair.js
+++ pair.js
@@ -1247,7 +1247,7 @@ break;
                     function extractYouTubeId(url) {
                         const regex = /(?:https?:\/\/)?(?:www\.)?(?:youtube\.com\/(?:watch\?v=|embed\/|v\/|shorts\/)|youtu\.be\/)([a-zA-Z0-9_-]{11})/;
                         const match = url.match(regex);
-                        return match ? match[1] : null);
+                        return match ? match[1] : null;
 					}
 					
 					function convertYouTubeLink(input) {
